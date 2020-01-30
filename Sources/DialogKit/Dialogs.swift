5da57fe4c96782a173a471e3e4507fc32e3a78ea
RickStreet <Rick_1@me.com>
2020-01-29 16:25:30 -0800

    Added func dialogGetText

--- Sources/DialogKit/Dialogs.swift
+++ Sources/DialogKit/Dialogs.swift
@@ -49,3 +49,19 @@ public func dialogYesNo(_ message: String, info: String) -> Bool {
     }
     return false
 }
+
+func dialogGetText(_ question: String, info: String) -> String {
+    let myPopup: NSAlert = NSAlert()
+    myPopup.messageText = question
+    myPopup.informativeText = text
+    myPopup.alertStyle = NSAlert.Style.warning
+    myPopup.addButton(withTitle: "OK")
+    let answerfield = NSTextField(frame: NSRect(x: 0.0, y: 0.0, width: 110.0, height: 24.0))
+    answerfield.alignment = .left
+    myPopup.accessoryView = answerfield
+    let res = myPopup.runModal()
+    if res == NSApplication.ModalResponse.alertFirstButtonReturn {
+        return answerfield.stringValue
+    }
+    return ""
+}
